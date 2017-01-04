8685d09fb0e817c14112ab9e78e775fd9307a440
arkhivania <arkhivania@gmail.com>
2017-01-04 21:37:14 +0300

    ps 2.0 support

--- Scripts/warm.ps1
+++ Scripts/warm.ps1
@@ -1,5 +1,20 @@
 ﻿while($true)
 {
-    Invoke-WebRequest -Uri http://www.multivox.ru:21111/nailhang/Interfaces
+    try
+    {        
+        $web = New-Object Net.WebClient
+        $res = $web.DownloadString("http://www.multivox.ru:21111/nailhang/Interfaces")
+        $web.Dispose()
+
+        $dt = Get-Date
+        if($res.Length -gt 10)
+        {            
+            "Request complete: $dt"
+        }
+    }catch
+    {
+        "Request failed: $dt"
+    }
+
     Start-Sleep -Seconds 10
 }
