741b32122df95d310d206c97f9985492cda56eed
arkhivania <arkhivania@gmail.com>
2017-08-20 16:17:43 +0300

    net core 2 migration 2

--- Nailhang.Web/Program.cs
+++ Nailhang.Web/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 
 namespace Nailhang.Web
 {
@@ -11,15 +12,27 @@ namespace Nailhang.Web
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls("http://0.0.0.0:5001/nailhang")
-                .UseIISIntegration()
+            BuildWebHost(args)
+                .Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5001/nailhang")
                 .Build();
 
-            host.Run();
-        }
+        //public static void Main(string[] args)
+        //{
+        //    var host = new WebHostBuilder()
+        //        .UseKestrel()
+        //        .UseContentRoot(Directory.GetCurrentDirectory())
+        //        .UseUrls("http://0.0.0.0:5001/nailhang")
+        //        .UseIISIntegration()
+        //        .UseStartup<Startup>()
+        //        .Build();
+
+        //    host.Run();
+        //}
     }
 }
