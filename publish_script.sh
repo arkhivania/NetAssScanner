af1da914ab26ebdb92d9d1a56b7f891717746c71
arkhivania <arkhivania@ubuntu-arkhivania-TC-Serv.domain.multivox.ru>
2018-06-15 20:03:01 +0300

    script update

--- publish_script.sh
+++ publish_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo systemctl stop nailhang_silo
 sudo systemctl stop nailhang_svn
 sudo systemctl stop nailhang
@@ -8,12 +7,11 @@ dotnet publish -c Release ./Nailhang.Silo/
 dotnet publish -c Release ./Nailhang.Web/
 dotnet publish -c Release ./Nailhang.Svn/
 
-cp prod_appsettings.json ./Nailhang.Web/bin/Release/netcoreapp2.0/publish/appsettings.json
-cp prod_appsettings.json ./Nailhang.Silo/bin/Release/netcoreapp2.0/publish/appsettings.json
-cp prod_appsettings.json ./Nailhang.Svn/bin/Release/netcoreapp2.0/publish/appsettings.json
+cp prod_appsettings.json ./Nailhang.Web/bin/Release/netcoreapp2.1/publish/appsettings.json
+cp prod_appsettings.json ./Nailhang.Silo/bin/Release/netcoreapp2.1/publish/appsettings.json
+cp prod_appsettings.json ./Nailhang.Svn/bin/Release/netcoreapp2.1/publish/appsettings.json
 
 sudo systemctl start nailhang_silo
 sudo systemctl start nailhang_svn
 sudo systemctl start nailhang
-
 
