f772fc5c7769c9d916b1104c6eb1baa2891de411
arkhivania <arkhivania@gmail.com>
2019-09-29 18:49:04 +0300

    пробуем PathBase

--- Nailhang.Blazor/Startup.cs
+++ Nailhang.Blazor/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Nailhang.Blazor.Data;
 using Nailhang.Display.MD5Cache;
@@ -64,9 +67,21 @@ namespace Nailhang.Blazor
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
+            var root = Configuration.GetSection("general")["wwwRoot"];
+            app.UsePathBase(new PathString(root));
             app.UseStaticFiles();
+            //app.UseHttpsRedirection();
+
+            
+
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+            //    RequestPath = new PathString(root)
+            //});
+
+            
 
             app.UseRouting();
 
