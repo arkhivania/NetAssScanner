0786a734e3ffa027e96814b6fba51f7f807c371a
arkhivania <arkhivania@gmail.com>
2019-09-30 11:11:47 +0300

    remove memory limit

--- Nailhang.Blazor/Docker/prepare.sh
+++ Nailhang.Blazor/Docker/prepare.sh
@@ -1,2 +1,2 @@
 docker build -t nailblazor .
-docker run -d -p 5003:5001 --restart always --memory="128m" --name nailblazor nailblazor
+docker run -d -p 5003:5001 --restart always --name nailblazor nailblazor
