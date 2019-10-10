137dcbd6d6234efabe3350d40dd137943f41100b
arkhivania <arkhivania@gmail.com>
2019-10-10 19:12:31 +0300

    небольшой интерфейс

--- Nailhang.Display/NetPublicSearch/Processing/NetSearch.cs
+++ Nailhang.Display/NetPublicSearch/Processing/NetSearch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Nailhang.Display.NetPublicSearch.Base;
 using Nailhang.Display.Tools.TextSearch.Base;
 using Nailhang.IndexBase.PublicApi;
@@ -10,7 +11,16 @@ namespace Nailhang.Display.NetPublicSearch.Processing
 {
     class NetSearch : Base.INetSearch
     {
-        readonly Base.SearchItem[] searchItems;
+        struct Searcher 
+        {
+            public Base.SearchItem[] SearchItems { get; set; }
+            public ISearch ClassSearch { get; set; }
+            public ISearch MethodSearch { get; set; }
+        }
+
+        readonly Task<Searcher> searcher;
+
+        
 
         static IEnumerable<Base.SearchItem> Prepare(AssemblyPublic assemblyPublic)
         {
@@ -30,41 +40,49 @@ namespace Nailhang.Display.NetPublicSearch.Processing
                 }
         }
 
-        readonly ISearch classSearch;
-        readonly ISearch methodSearch;
-
-        public NetSearch(IWSBuilder wSBuilder, IPublicApiStorage publicApiStorage)
+        Searcher Create(IWSBuilder wSBuilder, IPublicApiStorage publicApiStorage)
         {
-            searchItems = publicApiStorage.LoadAssemblies().SelectMany(a => Prepare(a))
+            var res = new Searcher();
+            res.SearchItems = publicApiStorage.LoadAssemblies().SelectMany(a => Prepare(a))
                 .ToArray();
 
             {
-                var search_strings = searchItems.Select(w => w.ClassName);
+                var search_strings = res.SearchItems.Select(w => w.ClassName);
                 var stat = wSBuilder.BuildStat(search_strings);
                 var index = wSBuilder.Index(search_strings.Select(w => new Bulk { Sentence = w }), stat);
-                this.classSearch = wSBuilder.CreateSearch(index);
+                res.ClassSearch = wSBuilder.CreateSearch(index);
             }
 
             {
-                var search_strings = searchItems.Select(w => w.MethodName);
+                var search_strings = res.SearchItems.Select(w => w.MethodName);
                 var stat = wSBuilder.BuildStat(search_strings);
                 var index = wSBuilder.Index(search_strings.Select(w => new Bulk { Sentence = w }), stat);
-                this.methodSearch = wSBuilder.CreateSearch(index);
+                res.MethodSearch = wSBuilder.CreateSearch(index);
             }
+
+            return res;
+        }
+
+        public NetSearch(IWSBuilder wSBuilder, IPublicApiStorage publicApiStorage)
+        {
+            this.searcher = Task.Factory
+                .StartNew(() => Create(wSBuilder, publicApiStorage), TaskCreationOptions.LongRunning);
         }
 
         public IEnumerable<SearchItem> Search(string query)
         {
+            var sr = searcher.Result;
+            
             var name = query;
-            return classSearch.Search(new Request { Message = query })
-                .Concat(methodSearch.Search(new Request { Message = query }))
+            return sr.ClassSearch.Search(new Request { Message = query })
+                .Concat(sr.MethodSearch.Search(new Request { Message = query }))
                 .OrderByDescending(w => w.Relevance)
-                .Where(w => w.Relevance > 0.75)
-                .Where(w => w.Relevance > 0.90f
-                        || searchItems[w.DocumentIndex].ClassName.ToLower().Contains(query)
-                        || searchItems[w.DocumentIndex].MethodName.ToLower().Contains(query))
-                .Select(q => searchItems[q.DocumentIndex])
-                .Take(100)
+                .Where(w => w.Relevance > 0.50)
+                .Where(w => w.Relevance > 0.75f
+                        || sr.SearchItems[w.DocumentIndex].ClassName.ToLower().Contains(query)
+                        || sr.SearchItems[w.DocumentIndex].MethodName.ToLower().Contains(query))
+                .Select(q => sr.SearchItems[q.DocumentIndex])
+                .Take(200)
                 .ToArray();
         }
     }
