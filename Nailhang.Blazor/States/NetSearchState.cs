cfe9a73299350fa8a66f1053c8d90094e9567314
arkhivania <arkhivania@gmail.com>
2019-10-21 12:31:32 +0300

    пилим дальше

--- Nailhang.Blazor/States/NetSearchState.cs
+++ Nailhang.Blazor/States/NetSearchState.cs
@@ -9,8 +9,58 @@ namespace Nailhang.Blazor.States
     public class NetSearchState
     {
         public string[] Namespaces { get; set; }
-        public string SelectedNamespace { get; set; }
-        public bool ShowOnlyPublic { get; set; }
+
+        string selectedNamespace;
+        private string query;
+        private bool showOnlyPublic;
+
+        public string SelectedNamespace
+        {
+            get => selectedNamespace;
+            set
+            {
+                if (selectedNamespace == value)
+                    return;
+
+                selectedNamespace = value;
+                if (SelectedNamespaceChanged != null)
+                    SelectedNamespaceChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler SelectedNamespaceChanged;
+
+        public bool ShowOnlyPublic 
+        {
+            get => showOnlyPublic;
+            set
+            {
+                if (showOnlyPublic == value)
+                    return;
+                showOnlyPublic = value;
+                if (ShowOnlyPublicChanged != null)
+                    ShowOnlyPublicChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler ShowOnlyPublicChanged;
+
         public ISearchItem[] DisplayItems { get; set; }
+
+        public string Query
+        {
+            get => query;
+            set
+            {
+                if (query == value)
+                    return;
+
+                query = value;
+                if (QueryChanged != null)
+                    QueryChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler QueryChanged;
     }
 }
